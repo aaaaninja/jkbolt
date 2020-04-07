878b1ef0a4eec36a4cf2c3d854c416201cf065a7
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-08 06:33:26 +0900

    こっちの方が簡潔

--- handler.ts
+++ handler.ts
@@ -3,12 +3,12 @@ import { App, ExpressReceiver } from '@slack/bolt'
 import { createServer, proxy } from 'aws-serverless-express'
 import 'source-map-support/register';
 
-const expressReceiver = new ExpressReceiver({
+const receiver = new ExpressReceiver({
     signingSecret: process.env.SLACK_SIGNING_SECRET
 })
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  receiver: expressReceiver
+  receiver
 })
 
-export const boltapp = (event: APIGatewayProxyEvent, _context: Context) => proxy(createServer(expressReceiver.app), event, _context)
+export const boltapp = (event: APIGatewayProxyEvent, _context: Context) => proxy(createServer(receiver.app), event, _context)
