12bcefb6249a58649a491d47b22ec85079107057
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-08 06:06:06 +0900

    雛形
    参考 (未検証) -> https://github.com/seratch/serverless-slack-bolt-aws

--- handler.ts
+++ handler.ts
@@ -1,12 +1,14 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { App, ExpressReceiver } from '@slack/bolt'
+import { createServer, proxy } from 'aws-serverless-express'
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-}
+const expressReceiver = new ExpressReceiver({
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+})
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: expressReceiver
+})
+
+export const boltapp = (event: APIGatewayProxyEvent, _context: Context) => proxy(createServer(expressReceiver.app), event, _context)
